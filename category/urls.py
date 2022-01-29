1483cf2eb9261827a41f7109e869b5a1540c39fd
itzomen <peng.akebuon2468@gmail.com>
2022-01-29 04:29:22 +0100

    updated endpoints

--- category/urls.py
+++ category/urls.py
@@ -3,8 +3,8 @@ from .views import  *
 
 
 urlpatterns = [
-    path("", create, name="create"),
-    path("", categories, name="categories"),
-    path("<str:pk>/", update, name="update"),
-    path("<str:pk>/", delete, name="delete"),
+    path("create", create, name="create"),
+    path("categories", categories, name="categories"),
+    path("update/<str:pk>/", update, name="update"),
+    path("delete/<str:pk>/", delete, name="delete"),
 ]
